8fd12f2da8a3355b9411c4f5a08256dd7c0d6e3a
Dechao <g@i3long.cn>
2018-09-19 10:22:53 +0800

    DNSProxy
    DNSProxy First Commit

---
+++ DNSProxy/DNSConfig.cs
@@ -0,0 +1,119 @@
+﻿using ARSoft.Tools.Net.Dns;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSProxy
+{
+    class DNSConfig
+    {
+        public DNSConfig()
+        {
+            InitTimer();
+        }
+        private DnsClient _dnsClient;
+        /// <summary>
+        /// 规则名字
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 匹配域名
+        /// </summary>
+        public string Domain { get; set; }
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public int Sort { get; set; }
+
+        /// <summary>
+        /// 获取服务信息
+        /// </summary>
+        public DnsClient ServerInfo
+        {
+            get
+            {
+                if (_dnsClient == null)
+                {
+                    SetDNSServerInfo();
+
+                }
+                return _dnsClient;
+            }
+        }
+
+        /// <summary>
+        /// 默认规则标识
+        /// </summary>
+        public bool IsDefault { get; set; }
+
+        /// <summary>
+        /// 设置服务器信息
+        /// </summary>
+        /// <param name="iP">IP address</param>
+        /// <param name="querytimeout">10</param>
+        private void SetDNSServerInfo()
+        {
+            if (string.IsNullOrEmpty(IP))
+                IP = "119.29.29.29"; //119.29.29.29 DNSPOD;  8.8.8.8 google
+
+            _dnsClient = new DnsClient(IPAddress.Parse(IP), QueryTimeout);
+        }
+
+
+        [DefaultValue(2)]
+        public int QueryTimeout { get; set; }
+
+        public string IP { get; set; }
+
+        /// <summary>
+        /// 服务是否正常连通（ping机制定时检测）
+        /// </summary>
+        [DefaultValue(true)]
+        public bool IsWork { get; set; }
+
+
+        #region Ping Timer
+
+        private System.Timers.Timer serverTimer = new System.Timers.Timer();
+        private void InitTimer()
+        {
+            IsWork = true;
+            serverTimer.Interval = 5000 + new Random().Next(1000);
+            serverTimer.Elapsed += new System.Timers.ElapsedEventHandler(ServerTimer_Elapsed);
+            serverTimer.Start();
+
+        }
+
+        private void ServerTimer_Elapsed(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(IP))
+                return;
+
+            Ping PNG = new Ping();
+            PingReply PNGRPLY = PNG.Send(IP, 2000);
+
+            if (PNGRPLY.Status == IPStatus.Success)
+            {
+                if (!IsWork)
+                    Console.WriteLine(Name + " Working");
+
+                IsWork = true;
+            }
+            else
+            {
+                IsWork = false;
+                Console.WriteLine(Name + " Can't Working!!!!");
+            }
+
+        }
+        #endregion
+
+    }
+}
---
+++ DNSProxy/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ARSoft.Tools.Net.Dns;
+using System.Net;
+using JsonConfig;
+
+
+namespace DNSProxy
+{
+    class Program
+    {
+        static IPAddress serverIP = IPAddress.IPv6Loopback;
+        static List<DNSConfig> dnsConfigs = new List<DNSConfig>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DNSProxy");
+
+            #region Config
+            Console.WriteLine("+Config Load...");
+
+            foreach (var item in Config.User.rules)
+            {
+                Console.WriteLine(item.Name);
+                dnsConfigs.Add(new DNSConfig() { Name = item.Name, Domain = item.Domain, IsDefault = item.IsDefault, IP = item.IP, Sort = item.Sort });
+            }
+
+            Console.WriteLine("+Config Load...OK");
+
+
+            #region 手动添加配置 取消
+            //dnsConfigs.Add(new DNSConfig() { Name = "Default", Domain = "", IsDefault = true, IP = "10.200.150.2" });//默认dns
+            //dnsConfigs.Add(new DNSConfig() { Name = "HomeDNS", Domain = "*", IsDefault = false, IP = "192.168.10.254" }); //全匹配dns
+            //dnsConfigs.Add(new DNSConfig() { Name = "XdfDNS.xdf", Domain = "xdf.cn", IsDefault = false, IP = "10.200.150.2" });//匹配域名xdf.n
+            //dnsConfigs.Add(new DNSConfig() { Name = "XdfDNS.neworiental", Domain = "neworiental.org", IsDefault = false, IP = "10.200.150.2" });//匹配域名neworiental.org
+            //dnsConfigs.Add(new DNSConfig() { Name = "r.i3long.cn", Domain = "r.i3long.cn", IsDefault = false, IP = "10.200.150.2" });//匹配域名r.i3long.cn
+            #endregion
+
+
+            #endregion
+
+            using (DnsServer server = new DnsServer(System.Net.IPAddress.Any, 53, 53))
+            {
+                server.QueryReceived += OnQueryReceived;
+
+                server.Start();
+
+                Console.WriteLine("Press Enter to stop server");
+                Console.ReadLine();
+            }
+
+
+        }
+
+        static async Task OnQueryReceived(object sender, QueryReceivedEventArgs e)
+        {
+            DnsMessage query = e.Query as DnsMessage;
+
+            if (query == null)
+                return;
+
+            DnsMessage response = query.CreateResponseInstance();
+            //response.AnswerRecords.Clear();
+            //response.AdditionalRecords.Clear();
+
+            DnsQuestion question = response.Questions.FirstOrDefault();
+
+            if (question == null || string.IsNullOrEmpty(question.Name.ToString()))
+                return;
+
+            #region rules match
+            //默认配置 无任何规则 满足最基础网络配置
+            var defaultConfig = dnsConfigs.Where(d => string.IsNullOrEmpty(d.Domain) && d.IsDefault).FirstOrDefault();
+
+
+            //全匹配规则配置文件，默认流量走向
+            var allInConfig = dnsConfigs.Where(d => d.Domain.Equals("*")).FirstOrDefault();
+
+            //当前匹配到的规则
+            DNSConfig currentConfig = null;
+
+            /*
+            // Console.WriteLine(dnsConfigs.Count());
+            Console.WriteLine(dnsConfigs
+                .Where(d => d.IsDefault == false && !d.Domain.Equals("*"))
+                .OrderByDescending(d => d.Sort)
+                .ToList().Count());
+            */
+            foreach (var item in dnsConfigs
+                .Where(d => d.IsDefault == false && !d.Domain.Equals("*"))
+                .OrderByDescending(d => d.Sort)
+                .ToList()
+                )
+            {
+                if (question.Name.ToString().Contains(item.Domain))
+                {
+                    currentConfig = item;
+                    break;
+                }
+            }
+
+
+            //ARecord ip = new ARecord(question.Name, 60, IPAddress.Parse("192.168.1.1"));
+            //response.AnswerRecords.Add(ip);
+
+            if (currentConfig == null)
+            {
+                if (allInConfig != null && allInConfig.IsWork)
+                {
+                    currentConfig = allInConfig;
+                    Console.WriteLine(question.Name.ToString() + " No rules match! Set * Config! ");
+                }
+                else
+                {
+                    currentConfig = defaultConfig;
+                    Console.WriteLine(question.Name.ToString() + " No rules match! Set Default Config! ");
+                }
+            } 
+            #endregion
+
+
+            Console.WriteLine(question.Name.ToString() + " ? " + currentConfig.Name + currentConfig.IP);
+
+            #region 获取解析
+            try
+            {
+                DnsMessage dnsMessage = currentConfig.ServerInfo.Resolve(question.Name, question.RecordType);
+                if ((dnsMessage == null) || ((dnsMessage.ReturnCode != ReturnCode.NoError) && (dnsMessage.ReturnCode != ReturnCode.NxDomain)))
+                {
+                    Console.WriteLine("???????");
+                    throw new Exception("DNS request failed");
+                }
+                else
+                {
+                    foreach (DnsRecordBase dnsRecord in dnsMessage.AnswerRecords)
+                    {
+                        try
+                        {
+                            ARecord aRecord = dnsRecord as ARecord;
+                            if (aRecord != null)
+                            {
+                                Console.WriteLine(question.Name.ToString() + " - " + currentConfig.Name + " " + aRecord.Address.ToString());
+                            }
+                        }
+                        catch { }
+
+                        response.AnswerRecords.Add(dnsRecord);
+                    }
+                }
+
+
+                response.ReturnCode = dnsMessage.ReturnCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                response.ReturnCode = ReturnCode.FormatError;
+            } 
+            #endregion
+
+            Console.WriteLine(question.Name.ToString() + " + " + currentConfig.Name + " " + response.ReturnCode);
+
+            e.Response = response;
+
+        }
+
+    }
+
+
+    
+}
